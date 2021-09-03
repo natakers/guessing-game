e4c739fb7beeb2a2f7be50465b52cfcf50adc71e
natakers <n.kersnovskaya@gmail.com>
2021-09-03 09:23:08 +0300

    feat: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,48 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max) {
+    	this.min = min;
+    	this.max = max;
+    }
 
     setRange(min, max) {
+    	// if (this.max )
+    	// this.max = (max+min)/2;
+    	this.max = max;
+    	this.min = min;
+    	console.log(this.max);
+    	return (this.max, this.max);
+
+    	// console.log((max-min)/2);
+    	// guess((max-min)/2);
+
 
     }
 
     guess() {
+    	
+    	// this.max = Math.ceil((this.max+this.min)/2);
+    	// console.log( "guess " + Math.ceil((this.max+this.min)/2));
+    	return Math.ceil((this.max+this.min)/2);
 
     }
 
     lower() {
 
+    	this.min = this.min;
+    	this.max = Math.ceil((this.max+this.min)/2);
+    	// console.log("lower max " + this.max);
+    	// console.log("lower min " + this.min);
+    	return  (this.min, this.max);
+    	// return  (this.max/2, this.min);
+
     }
 
     greater() {
+    	this.min = Math.ceil((this.max+this.min)/2);;
+    	this.max = this.max;
+    	// console.log("greater max " + this.max);
+    	// console.log("greater min " + this.min);
+    	return  (this.min, this.max);
 
     }
 }
